a68775a24aff949b922616748865d14b6c7a416c
hermlon <dev.hermlon@yamx.net>
2021-12-07 18:14:46 +0100

    Removed potentially appointement blocking api call

--- jellie.py
+++ jellie.py
@@ -4,8 +4,6 @@ import jellieconfig
 import asyncio
 import requests
 import time
-import itertools
-import operator
 from bs4 import BeautifulSoup
 from datetime import datetime
 
@@ -33,7 +31,7 @@ class JellieClient(discord.Client):
     async def refresh_info(self):
         await self.wait_until_ready()
         while not self.is_closed():
-            data = self.api.get_pretty_new_data()
+            data = self.api.get_new_data()
             if data:
                 await self.notify(data)
                 await self.update_status('New data')
@@ -45,16 +43,11 @@ class JellieClient(discord.Client):
     async def notify(self, data):
         message_str = ''
         if self.startup:
-            message_str = 'Aktuell verfügbare Termine:\n'
+            message_str = 'eventuell Termine verfügbar an folgenden Orten:\n'
         else:
-            message_str = '**__Neue Termine__** *(für Booster nach Zweifach-Impfung), Auszug*\n'
+            message_str = '**__eventuell neue Termine an folgenden Orten__** *(für Booster nach Zweifach-Impfung), Auszug*\n'
         for location in data:
-            message_str += '• {}\n'.format(location['location'])
-            for date in location['dates']:
-                message_str += '    - {}: '.format(date['date'])
-                for time in date['times']:
-                    message_str += ' `{}`'.format(time)
-                message_str += '\n'
+            message_str += '• {}\n'.format(location)
 
         # check if above character limit
         if len(message_str) >= 2000:
@@ -86,56 +79,15 @@ class Api:
     def __init__(self):
         self.cache = set()
 
-    def date_sort(date):
-        return date[1][-10:]
-
-    def get_pretty_new_data(self):
-        diff = self.get_new_data()
-        diff = sorted(diff, key=operator.itemgetter(0))
-        result = []
-        for location, entry3 in itertools.groupby(diff, operator.itemgetter(0)):
-            res = {'location': location, 'dates': []}
-            dates = sorted(entry3, key=Api.date_sort)
-            for date, times in itertools.groupby(dates, operator.itemgetter(1)):
-                d = {'date': date, 'times': []}
-                times = sorted(times, key=operator.itemgetter(2))
-                for time in times:
-                    d['times'].append(time[2])
-                res['dates'].append(d)
-            result.append(res)
-        return result
-
-
     def get_new_data(self):
         locations = filter(self.matches_town, self.get_locations())
         diff = set()
-        new_cache = set()
         for location in locations:
-            times = self.get_times(location['id'])
-            for time in times:
-                combined = (location['name'],) + time
-                if combined[0] not in self.cache:
-                    diff.add(combined)
-                new_cache.add(combined[0])
-        self.cache = new_cache
+            if location not in self.cache:
+                diff.add(location)
+        self.cache = locations
         return diff
 
-    def get_times(self, location):
-        try:
-            payload = {'typ': '1', 'ort': location, 'pos': '0', 'tmstmp': self.get_time()}
-            page = requests.post(jellieconfig.url, data=payload)
-            soup = BeautifulSoup(page.text, 'html.parser')
-
-            times = []
-
-            for day in soup.find_all('optgroup'):
-                for time in day.find_all('option'):
-                    times.append((day['label'], time.text[-5:]))
-            return times
-        except requests.exceptions.ConnectionError as e:
-            return None
-
-        
     def get_locations(self):
         try:
             payload = {'typ': '3', 'indi': '35', 'aktiv': '1', 'tmstmp': self.get_time()}
@@ -146,14 +98,14 @@ class Api:
 
             for loc in soup.find_all('option'):
                 if loc['value'] != '0':
-                    locations.append({'id': loc['value'], 'name': loc.text})
+                    locations.append(loc.text)
             return locations
         except requests.exceptions.ConnectionError as e:
             print(e)
             return []
 
     def matches_town(self, location):
-        return any([loc in location['name'] for loc in jellieconfig.matches])
+        return any([loc in location for loc in jellieconfig.matches])
 
     def get_time(self):
         return int(time.time()*1000)
